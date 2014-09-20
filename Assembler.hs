3cae49b2120f8fc7ec23ad06ab4fdefa68fb3002
Kathy Spradlin <kathyspradlin@gmail.com>
2014-09-20 18:43:05 -0500

    Reformat decoding

--- Assembler.hs
+++ Assembler.hs
@@ -11,9 +11,8 @@ import Data.Either
 import Control.Applicative
 import Numeric
 
-
+data Format = A String | B String | C String | Invalid
 data Var = Imm Int16 | RegID Int16 deriving (Show)
-data Assembly = Assembly {ins :: Instruction, vars :: [String]} deriving (Show)
 
 type MachineCode = String
 type Instruction = String
@@ -32,35 +31,42 @@ assembleToHex s = map toHex <$> assemble s
           toHex bits = (hexDigit $ take 4 bits) : (toHex $ drop 4 bits)
           hexDigit = (intToDigit . readImmBase 2)
 
-assembleLine :: String -> Either Error MachineCode
-assembleLine s = getIns $ Assembly instruction vars
-    where (instruction:vars) = words . addSpaces $ s
+assembleLine :: String -> Either String MachineCode
+assembleLine s =
+    case eitherVars of
+        Left errStr -> Left errStr
+        Right vars -> getIns (instruction, vars)
+    where eitherVars = mapM getVar vars
+          (instruction:vars) = words . addSpaces $ s
+
+getIns :: (Instruction, [Var]) -> Either Error MachineCode
+getIns ("add", (getFormat -> B varCode)) = Right $ "00000" ++ varCode
+getIns ("add", (getFormat -> A varCode)) = Right $ "00001" ++ varCode
+getIns ("call", (getFormat -> B varCode)) = Right $ "11010" ++ varCode
+getIns ("call", (getFormat -> C varCode)) = Right $ "11011" ++ varCode
+getIns ("slt", (getFormat -> B varCode)) = Right $ "00100" ++ varCode
+getIns ("slt", (getFormat -> A varCode)) = Right $ "00101" ++ varCode
+getIns ("brz", (getFormat -> B varCode)) = Right $ "11110" ++ varCode
+getIns ("brz", (getFormat -> C varCode)) = Right $ "11111" ++ varCode
+getIns ("lea", (getFormat -> B varCode)) = Right $ "11000" ++ varCode
+getIns ("lea", (getFormat -> C varCode)) = Right $ "11001" ++ varCode
+getIns ("shl", (getFormat -> B varCode)) = Right $ "10000" ++ varCode
+getIns ("shl", (getFormat -> A varCode)) = Right $ "10001"++ varCode
+getIns (_, (getEitherFormat -> Left err)) = Left err
+getIns asm = Left $ "Invalid instruction: " ++ show asm
     
-getIns :: Assembly -> Either Error MachineCode
-getIns (Assembly (getOp -> Right opCode) (getVarCode -> Right varCode)) = Right $ opCode ++ varCode
-getIns (Assembly (getOp -> Left errStr) _) = Left errStr
-getIns asm@(Assembly _ (getVarCode -> Left errStr)) = Left $ "Invalid Instruction" ++ show asm ++ " error: " ++ errStr
-getIns asm = Left $ "Invalid Instruction: " ++ show asm
+getFormat :: [Var] -> Format
+getFormat (getEitherFormat -> Right fmt) = fmt
+getFormat (getEitherFormat -> Left err) = Invalid
 
-getOp :: Instruction -> Either Error MachineCode
-getOp "add" = Right "0000"
-getOp "call" = Right "1101"
-getOp "slt" = Right "0010"
-getOp "brz" = Right "1111"
-getOp "lea" = Right "1100"
-getOp "shl" = Right "1000"
-getOp malformedOp = Left $ malformedOp ++ " is not a valid instruction"
-
-getVarCode :: [String] -> Either Error MachineCode
-getVarCode vars = case mapM getVar vars of
-    Right [RegID rd, RegID ra, RegID rb] -> buildBits <$> getReg rd <*> getReg ra <*> getReg rb
-        where buildBits dBits aBits bBits = "1" ++ dBits ++ aBits ++ "00" ++ bBits
-    Right [RegID rd, RegID ra, Imm imm5] -> buildBits <$> getReg rd <*> getReg ra <*> getImm5 imm5
-        where buildBits dBits aBits immBits = "0" ++ dBits ++ aBits ++ immBits
-    Right [RegID rd, Imm imm8] -> buildBits <$> getReg rd <*> getImm8 imm8
-        where buildBits dBits immBits = "1" ++ dBits ++ immBits
-    Right _ -> Left $ "Invalid Instruction: " ++ (show vars)
-    Left errStr -> Left errStr
+getEitherFormat :: [Var] -> Either Error Format
+getEitherFormat [RegID rd, RegID ra, RegID rb] = A <$> (buildBits <$> getReg rd <*> getReg ra <*> getReg rb)
+        where buildBits dBits aBits bBits = dBits ++ aBits ++ "00" ++ bBits
+getEitherFormat [RegID rd, RegID ra, Imm imm5] = B <$> (buildBits <$> getReg rd <*> getReg ra <*> getImm5 imm5)
+        where buildBits dBits aBits immBits = dBits ++ aBits ++ immBits
+getEitherFormat [RegID rd, Imm imm8] = C <$> (buildBits <$> getReg rd <*> getImm8 imm8)
+        where buildBits dBits immBits = dBits ++ immBits
+getEitherFormat vars = Left $ "Invalid format for vars: " ++ show vars
 
 getReg = getNLowestBits 3 False
 getImm5 = getNLowestBits 5 True
